7e33a1a71f6b7252c38afd357115cf26860658e4
Ather Ahmad <atherahmad49@yahoo.com>
2022-10-25 11:58:34 +0200

    bcryp implemented inside models

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,14 +2,16 @@
 import userModel from "../models/userModel.js";
 import { validationResult } from "express-validator";
 import bcrypt from "bcrypt"
+import UserModel from "../models/userModel.js";
 
 export const signin = async (req, res, next) => {
 
     const {email, password} = req.body;
 
-   
+    const user = await UserModel.verifyUser(email, password, next)
+    res.status(200).json(user)
     try {
-        const user = await userModel.findOne({email})
+       /*  const user = await userModel.findOne({email})
 
         const verified = await bcrypt.compare(password, user.password )
 
@@ -17,9 +19,9 @@ export const signin = async (req, res, next) => {
         error.status = 401
         if(!verified) throw error
         
-        /* if(!verified) return next({message:"Invalid Credentials", status:401}) */
+        // if(!verified) return next({message:"Invalid Credentials", status:401}) 
         
-        res.status(200).json({message: "successfully logged in"})
+        res.status(200).json({message: "successfully logged in"}) */
 
 
     } catch (error) {
@@ -29,18 +31,24 @@ export const signin = async (req, res, next) => {
 
   export const signup = async (req, res, next) => {
 
-    const {password} = req.body
+    const {password, email} = req.body
     try {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
           return res.status(422).json({ errors: errors.array() });
         }
-    
-        const saltCount = 10;
+       /*  const emailAvailable = await UserModel.findOne({email})
+        if(emailAvailable) {
+          const error = new Error("Email already registered")
+          error.status = 401
+          throw error 
+        }
+         */
+        /* const saltCount = 10;
         const salt = await bcrypt.genSalt(saltCount)
         const hashedPassword = await bcrypt.hash(password, salt  )
 
-        req.body.password = hashedPassword
+        req.body.password = hashedPassword */
 
         const user = new userModel(req.body);
         await user.save();
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 
 const UserSchema = mongoose.Schema({
   firstName: {
@@ -12,12 +13,54 @@ const UserSchema = mongoose.Schema({
   email: {
     type: String,
     required: true,
+    unique:true
+    
   },
   password: {
     type: String,
     required: true,
   },
 });
+
+UserSchema.pre("save", async function (next){
+
+  try{
+  const saltCount = 12
+  const salt = await bcrypt.genSalt(saltCount)
+  const hashedPassword = await bcrypt.hash(this.password, salt)
+  this.password = hashedPassword
+
+  next()
+
+}catch(err){
+  next(err)
+}
+})
+
+UserSchema.post("save", function(next){
+  console.log("document save")
+})
+
+
+UserSchema.statics.verifyUser = async (email, password, next) =>{
+
+try{
+    const user = await UserModel.findOne({email})
+  const verified = await bcrypt.compare(password, user.password)
+  if(!verified) {
+    const err = new Error("Credentials are invalid!")
+    err.status = 401
+    throw err
+  }
+  user.password = undefined
+  return user
+
+} catch(err){
+    next(err)
+  }
+
+}
+
 
 const UserModel = mongoose.model("User", UserSchema);
 
